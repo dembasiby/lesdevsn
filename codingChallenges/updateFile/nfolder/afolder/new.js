e08c2e0e4c95775a0806edf0bfd0a0cc1552976a
Demba Siby <dembasiby@gmail.com>
2018-08-13 09:10:18 +0000

    File updated

--- codingChallenges/updateFile/nfolder/afolder/new.js
+++ codingChallenges/updateFile/nfolder/afolder/new.js
@@ -1,7 +1,18 @@
+console.log(dupReq);
   return next.handle(dupReq)
     .catch( (err) => {
       if( err instanceof HttpErrorResponse) {
-                                                                                    
+        console.group("Error Instance");
+          console.log('Error Status:');
+          console.log(`${err.status} - ${err.error.message}`);
+          console.log('----------------');
+          console.log('Error Object:');
+          console.log(err);
+          console.log('--------------');
+        console.groupEnd();
+        console.log(`Something bad happened: ${err.status} - ${err.error.message}`);
+
         return Observable.throw(err.error);
       }
+
     });
